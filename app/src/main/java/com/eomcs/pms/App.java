f99d7ff1b3f348bad4cbde50dc9ebab6ddc49f4a
jisukb <wwrbrbww@gmail.com>
2021-01-19 03:45:31 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -1,43 +1,148 @@
 package com.eomcs.pms;
 
+import java.sql.Date;
 import java.util.Scanner;
 
 public class App {
+
+  static Scanner keyboardScan = new Scanner(System.in);
+
+  static final int LENGTH = 100;
+  static int[] no = new int[LENGTH];
+  static String[] name = new String[LENGTH];
+  static String[] email = new String[LENGTH];
+  static String[] password = new String[LENGTH];
+  static String[] photo = new String[LENGTH];
+  static String[] tel = new String[LENGTH];
+  static Date[] registeredDate = new Date[LENGTH];
+  static int size = 0;
+
+  static final int PLENGTH = 100;
+  static int[] pno = new int[PLENGTH];
+  static String[] ptitle = new String[PLENGTH];
+  static String[] pcontent = new String[PLENGTH];
+  static Date[] pstartDate = new Date[PLENGTH];
+  static Date[] pendDate = new Date[PLENGTH];
+  static String[] powner = new String[PLENGTH];
+  static String[] pmembers = new String[PLENGTH];
+  static int psize = 0;
+
+  static final int TLENGTH = 100;
+  static int[] tno = new int[TLENGTH];
+  static String[] tcontent = new String[TLENGTH];
+  static Date[] tdeadline = new Date[TLENGTH];
+  static String[] towner = new String[TLENGTH];
+  static int[] tstatus = new int[TLENGTH];
+  static int tsize = 0;
+
   public static void main(String[] args) {
-    System.out.println("[회원]");
 
-    Scanner keyScan = new Scanner(System.in);
+    while (true) {
+      String input = promptString("명령> ");
+      if (input.equalsIgnoreCase("exit") || 
+          input.equalsIgnoreCase("quit")) {
+        break;
+      } else if (input.equalsIgnoreCase("/member/add")) {
+        addMember();
+      } else if (input.equalsIgnoreCase("/member/list")) {
+        listMember();
+      } else if (input.equalsIgnoreCase("/project/add")) {
+        addProject();
+      } else if (input.equalsIgnoreCase("/project/list")) {
+        listProject();
+      } else if (input.equalsIgnoreCase("/task/add")) {
+        addTask();
+      } else if (input.equalsIgnoreCase("/task/list")) {
+        listTask();
+      } else {
+        System.out.println("실행할 수 없는 명령입니다.");
+      }
 
-    System.out.print("번호? ");
-    String no = keyScan.nextLine();
+      System.out.println();
+    }
 
-    System.out.print("이름? ");
-    String name = keyScan.nextLine();
+    keyboardScan.close();
+    System.out.println("안녕!");
+  }
+  static void addMember() {
+    System.out.println("[회원 등록]");
+    no[size] = promptInt("번호? ");
+    name[size] = promptString("이름? ");
+    email[size] = promptString("이메일? ");
+    password[size] = promptString("암호? ");
+    photo[size] = promptString("사진? ");
+    tel[size] = promptString("전화? ");
+    registeredDate[size] = new java.sql.Date(System.currentTimeMillis());
+    size++;
+  }
 
-    System.out.print("이메일? ");
-    String email = keyScan.nextLine();
+  static void listMember() {
+    System.out.println("[회원 목록]");
+    for (int i = 0; i < size; i++) {
+      System.out.printf("%d, %s, %s, %s, %s\n", 
+          no[i], name[i], email[i], tel[i], registeredDate[i]);
+    }
+  }
 
-    System.out.print("암호? ");
-    String password = keyScan.nextLine();
+  static void addProject() {
+    System.out.println("[프로젝트 등록]");
+    pno[psize] = promptInt("번호? ");
+    ptitle[psize] = promptString("프로젝트명? ");
+    pcontent[psize] = promptString("내용? ");
+    pstartDate[psize] = promptDate("시작일? ");
+    pendDate[psize] = promptDate("종료일? ");
+    powner[psize] = promptString("만든이? ");
+    pmembers[psize] = promptString("팀원? ");
+    psize++;
+  }
 
-    System.out.print("사진? ");
-    String photo = keyScan.nextLine();
+  static void listProject() {
+    System.out.println("[프로젝트 목록]");
+    for (int i = 0; i < psize; i++) {
+      System.out.printf("%d, %s, %s, %s, %s\n", 
+          pno[i], ptitle[i], pstartDate[i], pendDate[i], powner[i]);
+    }
+  }
 
-    System.out.print("전화? ");
-    String number = keyScan.nextLine();
+  static void addTask() {
+    System.out.println("[작업 등록]"); 
+    tno[tsize] = promptInt("번호? ");
+    tcontent[tsize] = promptString("내용? ");
+    tdeadline[tsize] = promptDate("마감일? ");
+    tstatus[tsize] = promptInt("상태?\n0: 신규\n1: 진행중\n2: 완료\n> ");
+    towner[tsize] = promptString("담당자? ");
+    tsize++;
+  }
 
-    java.sql.Date now = new java.sql.Date(System.currentTimeMillis());
+  static void listTask() {
+    System.out.println("[작업 목록]");
+    for (int i = 0; i < tsize; i++) {
+      String stateLabel = null;
+      switch (tstatus[tsize]) {
+        case 1:
+          stateLabel = "진행중";
+          break;
+        case 2:
+          stateLabel = "완료";
+          break;
+        default:
+          stateLabel = "신규";
+      }
+      System.out.printf("%d, %s, %s, %s, %s\n", 
+          tno[i], tcontent[i], tdeadline[i], towner[i], stateLabel);
+    }
+  }
 
-    keyScan.close();
+  static String promptString(String title) {
+    System.out.print(title);
+    return keyboardScan.nextLine();
+  }
 
-    System.out.println("--------------------------");
+  static int promptInt(String title) {
+    return Integer.parseInt(promptString(title));
+  }
 
-    System.out.println("번호: " + no);
-    System.out.println("이름: " + name);
-    System.out.println("이메일: " + email);
-    System.out.printf("암호: %s\n", password);
-    System.out.printf("사진: %s\n", photo);
-    System.out.printf("전화: %s\n", number);
-    System.out.printf("가입일: %s\n", now);
+  static Date promptDate(String title) {
+    return Date.valueOf(promptString(title));
   }
 }
